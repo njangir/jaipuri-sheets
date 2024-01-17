35f537171552001bdf0b199ba657bb0eba3a9f72
Naresh Jangir <njak46.jangir@gmail.com>
2024-01-17 19:28:41 +0000

    build: payload setup

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,10 +6,12 @@ import { Separator } from "./ui/separator"
 import { formatPrice } from "@/lib/utils"
 import { buttonVariants } from "./ui/button"
 import Link from "next/link"
+import Image from "next/image"
+
 
 const Cart = () => {
 
-    const itemCount = 1
+    const itemCount = 0
 
     const fee =1
 
@@ -48,7 +50,7 @@ const Cart = () => {
                                     Total
                                 </span>
                                 <span>{formatPrice(fee)}</span>
-                            </div>
+                            </div> 
                         </div>
                         <SheetFooter>
                             <SheetTrigger asChild>
@@ -58,8 +60,27 @@ const Cart = () => {
                     </div>
                 </>):(<div className="flex h-full flex-col items-center justify-center space-y-1">
                     <div className="relative mb-4 h-60 w-60 text-muted-foreground">
-                        <Image/>
+                        <Image
+                            src='/hippo-empty-cart.png'
+                            fill
+                            alt='empty shopping cart hippo'
+                        />
                     </div>
+                    <div className='text-xl font-semibold'>
+                    Your cart is empty
+                    </div>
+                    <SheetTrigger asChild>
+                        <Link
+                            href='/products'
+                            className={buttonVariants({
+                            variant: 'link',
+                            size: 'sm',
+                            className:
+                                'text-sm text-muted-foreground',
+                            })}>
+                            Add items to your cart to checkout
+                        </Link>
+                    </SheetTrigger>
                 </div>)}
             </SheetContent>
         </SheetTrigger>
