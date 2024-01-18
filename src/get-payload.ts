91072b0842972387e136bc615297bfe6ae84794e
Naresh Jangir <njak46.jangir@gmail.com>
2024-01-18 13:29:31 +0000

    build: added functionanlity verify email on sign ups

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,12 +2,13 @@ import dotenv from 'dotenv'
 import path from 'path'
 import type { InitOptions } from 'payload/config'
 import payload, { Payload } from 'payload'
+import nodemailer from 'nodemailer'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 })
 
-/* const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: 'smtp.resend.com',
   secure: true,
   port: 465,
@@ -15,7 +16,7 @@ dotenv.config({
     user: 'resend',
     pass: process.env.RESEND_API_KEY,
   },
-}) */
+})
 
 let cached = (global as any).payload
 
@@ -43,6 +44,11 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "onboarding@resend.dev",
+        fromName: "JD Industries",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { appRouter } from "./trpc"
+import { inferAsyncReturnType } from "@trpc/server"
 
 const app = express()
-
 const PORT = Number(process.env.PORT) || 3000
+
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+})
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
 const start =  async() => {
     const payload = await getPayloadClient({
@@ -17,14 +29,22 @@ const start =  async() => {
       })
 
     app.use((req, res) => nextHandler(req, res))
+    
+    app.use(
+      '/api/trpc',
+      trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+      })
+    )
 
     nextApp.prepare().then(() => {
-        payload.logger.info('Next.js started')
-
+/*         payload.logger.info('Next.js started')
+ */
         app.listen(PORT, async () => {
-            payload.logger.info(
+            /* payload.logger.info(
             `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-            )
+            ) */
         })
     })
 }
