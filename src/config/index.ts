3cb264ac6cfcae3514875954e941f8eac0007b8b
Naresh Jangir <njak46.jangir@gmail.com>
2024-01-23 10:02:29 +0530

    test: Category link corrected

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,17 +5,17 @@ export const PRODUCT_CATEGORIES = [
     featured: [
       {
         name: 'Dog Food',
-        href: `/products?category=ui_kits`,
+        href: `/products?category=dogs`,
         imageSrc: '/nav/ui-kits/mixed.jpg',
       },
       {
         name: 'Dog Grooming',
-        href: '/products?category=ui_kits&sort=desc',
+        href: '/products?category=dogs&sort=desc',
         imageSrc: '/nav/ui-kits/blue.jpg',
       },
       {
         name: 'Dog Accessories',
-        href: '/products?category=ui_kits',
+        href: '/products?category=dogs',
         imageSrc: '/nav/ui-kits/purple.jpg',
       },
     ],
@@ -26,17 +26,17 @@ export const PRODUCT_CATEGORIES = [
     featured: [
       {
         name: 'Cat Food',
-        href: `/products?category=icons`,
+        href: `/products?category=cats`,
         imageSrc: '/nav/icons/picks.jpg',
       },
       {
         name: 'Cat Grooming',
-        href: '/products?category=icons&sort=desc',
+        href: '/products?category=cats&sort=desc',
         imageSrc: '/nav/icons/new.jpg',
       },
       {
         name: 'Cat Accessories',
-        href: '/products?category=icons',
+        href: '/products?category=cats',
         imageSrc: '/nav/icons/bestsellers.jpg',
       },
     ],
