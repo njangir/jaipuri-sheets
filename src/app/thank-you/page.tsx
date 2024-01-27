183a94b3b31858c920a9da5c726a7f79675e1a65
Naresh Jangir <njak46.jangir@gmail.com>
2024-01-28 00:45:46 +0530

    build: removed Product file references

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image'
 import { cookies } from 'next/headers'
 import { getPayloadClient } from '@/get-payload'
 import { notFound, redirect } from 'next/navigation'
-import { Product, ProductFile, User } from '@/payload-types'
-import { PRODUCT_CATEGORIES } from '@/config'
+import { Product, User } from '@/payload-types'
 import { formatPrice } from '@/lib/utils'
 import Link from 'next/link'
 import PaymentStatus from '@/components/PaymentStatus'
@@ -101,67 +100,6 @@ const ThankYouPage = async ({
               <div className='mt-2 text-gray-900'>
                 {order.id}
               </div>
-
-              {/* <ul className='mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-muted-foreground'>
-                {(order.products as Product[]).map(
-                  (product) => {
-                    const label = PRODUCT_CATEGORIES.find(
-                      ({ value }) =>
-                        value === product.category
-                    )?.label
-
-                    const downloadUrl = (
-                      product.product_files as ProductFile
-                    ).url as string
-
-                    const { image } = product.images[0]
-
-                    return (
-                      <li
-                        key={product.id}
-                        className='flex space-x-6 py-6'>
-                        <div className='relative h-24 w-24'>
-                          {typeof image !== 'string' &&
-                          image.url ? (
-                            <Image
-                              fill
-                              src={image.url}
-                              alt={`${product.name} image`}
-                              className='flex-none rounded-md bg-gray-100 object-cover object-center'
-                            />
-                          ) : null}
-                        </div>
-
-                        <div className='flex-auto flex flex-col justify-between'>
-                          <div className='space-y-1'>
-                            <h3 className='text-gray-900'>
-                              {product.name}
-                            </h3>
-
-                            <p className='my-1'>
-                              Category: {label}
-                            </p>
-                          </div>
-
-                          {order._isPaid ? (
-                            <a
-                              href={downloadUrl}
-                              download={product.name}
-                              className='text-amber-600 hover:underline underline-offset-2'>
-                              Download asset
-                            </a>
-                          ) : null}
-                        </div>
-
-                        <p className='flex-none font-medium text-gray-900'>
-                          {formatPrice(product.price)}
-                        </p>
-                      </li>
-                    )
-                  }
-                )}
-              </ul> */}
-
               <div className='space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground'>
                 <div className='flex justify-between'>
                   <p>Subtotal</p>
